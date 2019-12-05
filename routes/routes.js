17e0561022d88c1edb5f004c6a388a6d5bcfea4f
nmolstad <nashmolstad@gmail.com>
2019-12-05 08:36:49 -0700

    uhhh dont remember

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
 
-const saltRounds = 15;
+const saltRounds = 5;
 
 mongoose.connect('mongodb+srv://express:Paf8PpfyCneqkqed@expresssite-knif3.mongodb.net/MTM282Final?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -16,7 +16,11 @@ const userSchema = mongoose.Schema({
     password: String,
     email: String,
     age: Number,
-    roles: Array
+    roles: Array,
+    active: Boolean,
+    answer1: String,
+    answer2: String,
+    answer3: String,
 });
 
 const User = mongoose.model("User", userSchema);
@@ -97,7 +101,7 @@ router.route("/create-account").post(
             answer3: req.body.question3
         }
         console.log
-        createUser(userInfo).then(function(result) {
+        createUser(userInfo, answers).then(function(result) {
             if (!result) {
                 req.session.username = userInfo.username;
                 resp.redirect("/");
@@ -114,7 +118,7 @@ router.route("/create-account").post(
     }
 )
 
-async function createUser(userInfo) {
+async function createUser(userInfo, answers) {
     var user = await User.findOne({ username: userInfo.username });
     if (user) {
         return userInfo.username + " already exists.";
@@ -125,7 +129,10 @@ async function createUser(userInfo) {
                 email: userInfo.email,
                 username: userInfo.username,
                 password: result,
-                roles: ['user']
+                roles: ['user'],
+                answer1: answers.answer1,
+                answer2: answers.answer2,
+                answer3: answers.answer3
             });
 
             user.save();
@@ -134,56 +141,62 @@ async function createUser(userInfo) {
 }
 
 async function saveAnswers(answers) {
-    var question1 = await Question.findOne({ name: "question1" });
-    switch (answers.answer1) {
-        case "1":
-            question1.answer1 += 1;
-            break;
-        case "2":
-            question1.answer2 += 1;
-            break;
-        case "3":
-            question1.answer3 += 1;
-            break;
-        case "4":
-            question1.answer4 += 1;
-            break;
+    if (answers.answer1) {
+        var question1 = await Question.findOne({ name: "question1" });
+        switch (answers.answer1) {
+            case "blue":
+                question1.answer1 += 1;
+                break;
+            case "green":
+                question1.answer2 += 1;
+                break;
+            case "red":
+                question1.answer3 += 1;
+                break;
+            case "purple":
+                question1.answer4 += 1;
+                break;
+        }
+        question1.save();
     }
-    question1.save();
 
-    var question2 = await Question.findOne({ name: "question2" });
-    switch (answers.answer2) {
-        case "1":
-            question2.answer1 += 1;
-            break;
-        case "2":
-            question2.answer2 += 1;
-            break;
-        case "3":
-            question2.answer3 += 1;
-            break;
-        case "4":
-            question2.answer4 += 1;
-            break;
+    if (answers.answer2) {
+        var question2 = await Question.findOne({ name: "question2" });
+        switch (answers.answer2) {
+            case "italian":
+                question2.answer1 += 1;
+                break;
+            case "asian":
+                question2.answer2 += 1;
+                break;
+            case "mexican":
+                question2.answer3 += 1;
+                break;
+            case "moroccan":
+                question2.answer4 += 1;
+                break;
+        }
+        question2.save();
     }
-    question2.save();
 
-    var question3 = await Question.findOne({ name: "question3" });
-    switch (answers.answer1) {
-        case "1":
-            question3.answer1 += 1;
-            break;
-        case "2":
-            question3.answer2 += 1;
-            break;
-        case "3":
-            question3.answer3 += 1;
-            break;
-        case "4":
-            question3.answer4 += 1;
-            break;
+    if (answers.answer3) {
+        var question3 = await Question.findOne({ name: "question3" });
+        switch (answers.answer3) {
+            case "dog":
+                question3.answer1 += 1;
+                break;
+            case "cat":
+                question3.answer2 += 1;
+                break;
+            case "sloth":
+                question3.answer3 += 1;
+                break;
+            case "eagle":
+                question3.answer4 += 1;
+                break;
+        }
+        question3.save();
     }
-    question3.save();
 }
 
 module.exports = router;
